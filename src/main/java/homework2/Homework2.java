c59cb39a8530b13c5c7758c54636d1a8783bfd3a
Атанас Туманов <atanastumanov@gmail.com>
2022-12-12 10:37:01 +0200

    Initial commit

--- src/main/java/homework2/Homework2.java
+++ src/main/java/homework2/Homework2.java
@@ -5,21 +5,38 @@ import java.util.Scanner;
 public class Homework2 {
     public static void main(String[] args){
 
+        // task 1
+
         String inputName = " Your name is <My name>";
+
         Scanner scan = new Scanner(System.in);
         String outputName = scan.nextLine();
+
         System.out.println(inputName.replace("<My name>", outputName));
+
+        // task 2
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your name"); //Milena
+        System.out.println("Enter your name");
         String firstName = scanner.nextLine();
 
-        System.out.println("Enter your username"); //milena
+        System.out.println("Enter your username");
         String userName = scanner.nextLine();
 
-        System.out.println(firstName.equalsIgnoreCase(userName)); // output is true
+        System.out.println(firstName.equalsIgnoreCase(userName));
+
+        // task 3
+
+        String cars = "I like BMW";
+        int count = cars.split("\\s").length;
+        System.out.println(count); // count is 3
+
+        // task 4
+
+        String color = "Red";
+        String car = "Ferrari";
+        System.out.println(color + " " + car);
+
     }
-
-
 }
